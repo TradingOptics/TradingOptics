71983ac0d636aa8a6c4936e0115e708673b03dad
TradingOptics <EagleDmos@gmail.com>
2025-09-12 20:49:12 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const container = document.getElementById("root");
-if (!container) {
-  throw new Error("Root container #root not found");
-}
-createRoot(container).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
